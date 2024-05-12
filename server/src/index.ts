154bfc1456aaf583e80d1ef4a8bdabc1b43a89ef
JosseMontano <josemariazambranamontano123@gmail.com>
2024-05-12 15:52:08 -0400

    bk:config of gl

---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+import { startApolloServer } from "./app";
+import { resolvers } from "./graphql/resolvers";
+import { typeDefs } from "./graphql/typeDefs";
+
+
+startApolloServer(typeDefs, resolvers);
+
